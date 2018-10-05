62b5215e01b31c6460835707c431ec49f1d17b7a
AkhilaMangipudi <akhila1012@gmail.com>
2018-10-04 23:34:49 -0500

    Homework 3 submission

---
+++ rottenpotatoes/spec/factories/movies_factory.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :movie do |f|
+    f.title "Spiderman"
+    f.rating "G"
+    f.description "Spider man movie"
+    f.release_date "12-12-12"
+    f.director "James"
+  end
+end
